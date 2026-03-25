0558076a9cf6cafa83b59613127929490398662c
yperezg01 <yperezg01@educantabria.es>
2026-03-25 16:29:42 +0100

    realizado en github
    realizado en github

--- ExamenGit/src/main/java/examen/examengit/ExamenGit.java
+++ ExamenGit/src/main/java/examen/examengit/ExamenGit.java
@@ -13,5 +13,6 @@ public class ExamenGit {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Yahir Pérez");
+        System.out.println("Desde github")
     }
 }
